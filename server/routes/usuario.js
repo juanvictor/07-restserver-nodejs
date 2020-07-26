f34ab78a79224b4aa7c82cf06faf98908f2c1071
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-07-25 23:17:29 -0400

    100 metodo GET usuario para listar todos los usuarios.

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
 
 const app = express();
 
 app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({})
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                usuarios
+            });
+        });
+
+    // res.json('get Usuario');
 });
 
 
@@ -51,7 +76,10 @@ app.post('/usuario', function(req, res) {
 
 app.put('/usuario/:id', function(req, res) {
     let id = req.params.id;
-    let body = req.body;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    // delete body.password;
+    // delete body.google;
 
     // METODO 1
     // Usuario.findById( id, (err, usuarioDB) => {});
