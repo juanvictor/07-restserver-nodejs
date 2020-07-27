7a86371c21b6746fc536199b3c526544aaaa355a
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-07-26 20:01:07 -0400

    113 Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:X1Yvm4LZWzNwKz6F@cluster0.wpj7w.mongodb.net/cafe';
+    // urlDB = 'mongodb+srv://strider:X1Yvm4LZWzNwKz6F@cluster0.wpj7w.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
